63d7fb24e8ab92b6e5e248ac117978a0f9d10610
tylerferg <35278888+tylerferg@users.noreply.github.com>
2018-01-16 07:22:00 -0700

    Added files through upload
    Node modules were excluded as the file was too large. This is simply for code review of a project I am currently working on.

---
+++ Microsoft-Project/src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAgXzjSzgAE5j1klkzdchNoRMKKnCwzXKA",
+    authDomain: "mpp-project-51762.firebaseapp.com",
+    databaseURL: "https://mpp-project-51762.firebaseio.com",
+    projectId: "mpp-project-51762",
+    storageBucket: "mpp-project-51762.appspot.com",
+    messagingSenderId: "785591582716" 
+  }
+};
